dd465425f16ea8b5c62cc34ae892ea34bc721c46
Claudio Melani <claudiomelani@imac-di-claudio-melani.local>
2017-05-27 18:41:50 +0200

    fix initial distribution for dealer

--- blackjack/gioco.py
+++ blackjack/gioco.py
@@ -3,8 +3,6 @@ import carta
 import mazzo
 import giocatore
 import mazziere
-import readline
-
 
 
 ListaSemi = ["Fiori", "Quadri", "Cuori", "Picche"]
@@ -30,12 +28,16 @@ ListaValori = {
 }
 
 
-def distribuisciIniziale(_mazzo, _giocatore,
-                         _mazziere):  # metodo distribuzione carte
+def distribuisciIniziale(_mazzo, _giocatore, _mazziere):  # metodo distribuzione carte
     if isinstance(_mazzo, mazzo.mazzo):  # controllo sulle instanze di classe
+
         if isinstance(_giocatore, giocatore.giocatore):
             for i in range(2):
                 _giocatore.riceviCarta(_mazzo.mazzo[i], _mazzo)
+
+        if isinstance(_mazziere,mazziere.mazziere):
+            for i in range(2):
+                _mazziere.riceviCarta(_mazzo.mazzo[i], _mazzo)
 
 
 def distribuisciCarta(_mazzo, _giocatore):  # metodo "carta"
@@ -152,53 +154,4 @@ def main():
 while True:
     main()
 
-#-------------------------------------------------------------------------
-# 23/01/17 18:30
-# 1 -   trovato problema su controllo assi, se somma sfora ma è presente un asso in modo
-#       da transformare valore in 1 da comunque sforato ->
-#       da implementare quando sfora un controllo e cambiare il valore di tutti gli assi in 1 ->
-#       non tutti solo quelli che compromettono la somma
-#-------------------------------------------------------------------------
 
-#-------------------------------------------------------------------------
-# 23/01/17 23:30
-# 1 -   risolto in modo parziale problema su gli assi ma si presenta di conseguenza un secondo problema
-#       quando, dopo aver compiuto in modo corretto il controllo su gli assi, riscontra problemi
-# nel aggiungere i successivi valori -> probabilmente problema logico ->
-# forse dovuto alla lista dei punti
-
-# 2 -   da implementare la classe mazziere, probabilmente simile a giocatore e rivedere il metodo della distribuzione
-#       iniziale(se ho voglia!)
-
-# 3 -   da implementare una componente con un patrimonio per non star ogni volta a riavviare in modo forzato lo script
-#-------------------------------------------------------------------------
-
-#-------------------------------------------------------------------------
-# 24/01/17 00:00
-# 1 -   problema rilevato nel cotrollo degli assi sembra compleatamente sistemato - auguri a me!
-#-------------------------------------------------------------------------
-
-#-------------------------------------------------------------------------
-# 24/01/17 8:20
-# 1 -	problema relativo nel controllo degli assi sembra essere ancora persistente
-#-------------------------------------------------------------------------
-
-#-------------------------------------------------------------------------
-# 24/01/17 9:10
-# 1 -	il problema dell'asso sembra essere risolto, altri test e posso passare all'implementazione
-#		del mazziere
-#-------------------------------------------------------------------------
-
-#-------------------------------------------------------------------------
-# 27/01/17 1:45
-# 1 -   i test sul controllo dell'asso sembrano andare bene posso iniziare l'implementazione
-#       dell'IA del mazziere -> sarà da riguardare anche la distribuzione delle carte all'inizio
-#       della partita in modo da dividere le prime due carte tra mazziere e giocatore (la prima
-#       al mazziere sarà coperta) e le altre due
-#-------------------------------------------------------------------------
-
-#-------------------------------------------------------------------------
-# 15/02/17 14:30
-# 1-	ho optato per la distribuzione non mischiata ma dare prima tutte le carte al giocatore e
-# 		successivamente al mazziere
-#-------------------------------------------------------------------------
--- blackjack/mazziere.py
+++ blackjack/mazziere.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+import carta
+
 class mazziere:
 
     def __init__(self):
